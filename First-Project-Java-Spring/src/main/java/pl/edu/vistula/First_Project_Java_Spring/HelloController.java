6f539b1fbcab2b404379b451b2bc597573a47422
KennethCarlos <157500602+KennethCarlos@users.noreply.github.com>
2025-05-19 00:10:15 +0200

    Update HelloController.java

--- First-Project-Java-Spring/src/main/java/pl/edu/vistula/First_Project_Java_Spring/HelloController.java
+++ First-Project-Java-Spring/src/main/java/pl/edu/vistula/First_Project_Java_Spring/HelloController.java
@@ -5,12 +5,14 @@ import org.springframework.web.bind.annotation.RestController;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RequestParam;
 
 
 @Controller
 public class HelloController {
 
     @GetMapping(value = "/")
+    @ResponseBody
     public String hello() {
         return "Hello Vistula";
     }
